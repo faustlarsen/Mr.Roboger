d840ec99b334514e1de7671a9b612fced0e14f45
faustlarsen <faustlarsen@gmail.com>
2020-10-31 11:44:37 -0700

    add reverse button reverse the output from big to small digit cap first char of each string in the name input form

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,18 @@ $(document).ready(function() {
   $("#pickNumber").submit(function(event) {
     event.preventDefault();
     let name = $("#inputName").val();
+    const upper = name.split(" ");
+    for (let i = 0; i < upper.length; i++) {
+    upper[i] = upper[i][0].toUpperCase() + upper[i].substr(1);
+  }
+    let final = upper.join(" ");
+
     let number = $("#inputNumber").val();
     let array = [];
     for (let i = 0; i <= number; i++) {
       let outcome = i.toString();
       if (outcome.includes(3)) {
-        array.push(" Won't you be my neighbor' " + ", " + name + "? ");
+        array.push(" Won't you be my neighbor' " + ", " + final + "? ");
       } else if (outcome.includes(2)) {
         array.push(' "Boop!" ');
       } else if (outcome.includes(1)) {
@@ -16,6 +22,10 @@ $(document).ready(function() {
         array.push(outcome);
       }
     }
+
     $("#output").text(array);
+    $("#reverse").click(function() {
+      $("#output").text(array.reverse());
+    });
   });
 });
